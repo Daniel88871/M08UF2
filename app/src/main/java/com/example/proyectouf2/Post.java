5fc1a397744b9b05b5355a24bbe3ac620ccbfa50
Daniel88871 <druizl01@elpuig.xeill.net>
2023-01-24 16:52:40 +0100

    hola

---
+++ app/src/main/java/com/example/proyectouf2/Post.java
@@ -0,0 +1,23 @@
+package com.example.proyectouf2;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class Post {
+    public String uid;
+    public String author;
+    public String authorPhotoUrl;
+    public String content;
+
+    // Constructor vacio requerido por Firestore
+    public Post() {}
+
+    public Map<String, Boolean> likes = new HashMap<>();
+
+    public Post(String uid, String author, String authorPhotoUrl, String content) {
+        this.uid = uid;
+        this.author = author;
+        this.authorPhotoUrl = authorPhotoUrl;
+        this.content = content;
+    }
+}
